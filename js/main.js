92be7be9ec6d7ad14ffa91e13c939a5798086a00
TyroneGithub <tyrone.stamaria35@gmail.com>
2020-04-19 22:34:03 +0800

    added table and how to

--- js/main.js
+++ js/main.js
@@ -1,5 +1,44 @@
+function createTbody(){
+    let table = document.querySelector('table');
+    let tbody = document.createElement('tbody');
+    table.appendChild(tbody);
+}
 
+function displayTable(str1, str2, key, ope){
+   let computation = document.querySelector('.computation');
+   let tbody = document.querySelector('tbody');
+   let tr = document.createElement('tr');
+   let td = [];
+   computation.style.display = "block";
+   if(ope === '+'){
+       document.querySelector('.p-text').innerHTML = "PLAIN TEXT";
+       document.querySelector('.p-text2').innerHTML = "CIPHERED TEXT";
 
+   }
+   else if(ope === '-'){
+    document.querySelector('.p-text').innerHTML = "CIPHERED TEXT";
+    document.querySelector('.p-text2').innerHTML = "PLAIN TEXT";
+   }
+   
+   for(let i = 0; i < 4; i++){
+    td[i] = document.createElement('td');
+    tr.appendChild(td[i]);
+   }
+   let tdText = document.createTextNode(String.fromCharCode(str1) + " (" + (str1 - 65) +")");
+   let keyText = document.createTextNode(String.fromCharCode(key) + " ("+ ope + (key - 65) + "mod26" + ")");
+   let result = document.createTextNode(str2);
+   let tdText2 = document.createTextNode(String.fromCharCode(str2 + 65) + " (" + (str2) + ")");
+   td[0].appendChild(tdText);
+   td[1].appendChild(keyText);
+   td[2].appendChild(result);
+   td[3].appendChild(tdText2);
+   tbody.appendChild(tr);
+  
+}
+function resetTable(){
+    let tbody = document.querySelector("tbody");
+    tbody.remove();
+}
 
 function cipher(){
     let key = document.querySelector('#key').value.toUpperCase();
@@ -7,16 +46,23 @@ function cipher(){
     let cipherText =  document.querySelector('#cipher-text1');
     let displayCipher = ""; 
     let keyCtr = 0;
+    resetTable();
+    createTbody();
     for(let i = 0; i < plainText.length; i++){    
         if(keyCtr == key.length){
             keyCtr = 0;
         }    
-        let cipherCode= (plainText.charCodeAt(i) + key.charCodeAt(keyCtr)) % 26;
-        cipherCode += 65;
-        keyCtr++;
-        let cipherChar =  String.fromCharCode(cipherCode);
-        displayCipher += cipherChar;
-
+        if(plainText.charAt(i) != " "){
+            let cipherCode= (plainText.charCodeAt(i) + key.charCodeAt(keyCtr)) % 26;
+            displayTable(plainText.charCodeAt(i), cipherCode, key.charCodeAt(keyCtr), '+');
+            cipherCode += 65;
+            keyCtr++;
+            let cipherChar =  String.fromCharCode(cipherCode);
+            displayCipher += cipherChar;
+        }
+        else{
+            displayCipher += " ";
+        }
     }
     cipherText.value = displayCipher;
    
@@ -29,18 +75,24 @@ function decipher(){
     let cipherText =  document.querySelector('#cipher-text2').value.toUpperCase();
     let displayPlain = ""; 
     let keyCtr = 0;
+    resetTable();
+    createTbody();
     for(let i = 0; i < cipherText.length; i++){    
         if(keyCtr == key.length){
             keyCtr = 0;
         }    
-        let plainCode= (cipherText.charCodeAt(i) - key.charCodeAt(keyCtr) + 26) % 26;
-        
+        if(cipherText.charAt(i) != " "){
+            let plainCode= (cipherText.charCodeAt(i) - key.charCodeAt(keyCtr) + 26) % 26;
+            displayTable(cipherText.charCodeAt(i), plainCode, key.charCodeAt(keyCtr), '-');
 
-        plainCode += 65;
-        keyCtr++;
-        let plainChar =  String.fromCharCode(plainCode);
-        displayPlain += plainChar;
-        
+            plainCode += 65;
+            keyCtr++;
+            let plainChar =  String.fromCharCode(plainCode);
+            displayPlain += plainChar;
+        }
+        else{
+            displayPlain += " ";
+        }
 
     }
     plainText.value = displayPlain;
